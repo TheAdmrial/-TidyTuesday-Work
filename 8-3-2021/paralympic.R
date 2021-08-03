7dfb4174f02faa7f8c012da4877c198de3ed29da
TheAdmrial <adamromriell@gmail.com>
2021-08-03 12:23:23 -0600

    starting work on tidy tuesday

---
+++ 8-3-2021/paralympic.R
@@ -0,0 +1,7 @@
+# load in libraries
+library(tidyverse)
+
+# read in the data
+athletes <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-08-03/athletes.csv')
+
+View(athletes)

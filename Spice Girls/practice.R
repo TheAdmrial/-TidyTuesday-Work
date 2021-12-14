2fe7ae5d7164f282c1c7c52f62ac4fc67cc2e39c
TheAdmrial <adamromriell@gmail.com>
2021-12-14 16:06:38 -0700

    just started

---
+++ Spice Girls/practice.R
@@ -0,0 +1,5 @@
+# load libraries
+library(tidyverse)
+
+# load in the data
+studio_album_tracks <- readr::read_csv("https://github.com/jacquietran/spice_girls_data/raw/main/data/studio_album_tracks.csv")

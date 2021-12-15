d8903ee3816c29d45da4ce5f2091d473db361fe1
TheAdmrial <adamromriell@gmail.com>
2021-12-15 16:04:58 -0700

    adding ideas of what to work on

--- Spice Girls/practice.R
+++ Spice Girls/practice.R
@@ -3,3 +3,8 @@ library(tidyverse)
 
 # load in the data
 studio_album_tracks <- readr::read_csv("https://github.com/jacquietran/spice_girls_data/raw/main/data/studio_album_tracks.csv")
+
+# what do I want to look at with the spice girls?
+# dance music? dance and sing music?
+# common words/topics?
+# 

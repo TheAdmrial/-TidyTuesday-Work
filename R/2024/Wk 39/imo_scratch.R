d4c4f9c2893757b5662da59531cbd1c5594f34b8
TheAdmrial <adamromriell@gmail.com>
2024-10-02 22:08:07 -0600

    Finishing up work on my TidyTuesday Graph and adding the resulting graph to the folder.

--- R/2024/Wk 39/imo_scratch.R
+++ R/2024/Wk 39/imo_scratch.R
@@ -15,8 +15,7 @@ View(country_results_df)
 ###### Graphing How country rankings shifted over time ######
 ## cleaning the data ##
 
-# imo_country_data <- 
-country_results_df %>% 
+imo_country_data <- country_results_df %>%
   select(
     c('year'
      , 'country'
@@ -37,6 +36,36 @@ country_results_df %>%
        , 'team_size_all')
   ) %>% 
   filter(!is.na(imo_total_score)) %>% 
-  group_by(year, country) %>% 
-  slice_head(imo_total_score, n = 5) #%>%
-  View()
+  group_by(year) %>% 
+  slice_max(imo_total_score
+            , n = 3
+            , na_rm = TRUE
+            , with_ties = TRUE) %>% 
+  ungroup() #%>% 
+
+###### Making the time series graph ######
+
+top_countries <- c('United States of America' = '#377eb8'
+                   , 'Russian Federation' = '#4daf4a'
+                   , "People's Republic of China" = '#e41a1c'
+                   #, 'Republic of Korea' = '#984ea3'
+                   )
+  
+  
+imo_country_data %>% 
+    filter(country %in% c('United States of America'
+                          , 'Russian Federation'
+                          , "People's Republic of China"
+                          # , 'Republic of Korea'
+                          )) %>% 
+  ggplot(aes(x = year, y = imo_total_score, color = country)) +
+  geom_jitter(width = 0.2, height = 0) +
+  geom_line(aes(color = country, group = country)) +
+  scale_fill_manual(values=top_countries) +
+  theme_minimal() +
+  labs(title = "International Mathematic Olymipad (IMO) Total Scores Over Time by Country"
+       , x = "Year"
+       , y = "IMO Total Score"
+       , color = "Country"
+       , caption = "The total score was calculated by assigning 4, 3, 2, and 1 point values\nto the number of Gold, Silver, Bronze, and Honorable Mentions won.\nThis was an arbitrary value assignment.")
+
